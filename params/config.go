90e5785d9e436af4dcc6affc9ca6136565857eb6
Ian Norden <iannordenn@gmail.com>
2019-12-06 14:00:20 -0600

    IsEWASM after IsEIP1559 forks in config so we don't need EWASM engine for EIP1559 tests; gasPrice in tx needs to not be required

--- params/config.go
+++ params/config.go
@@ -66,9 +66,9 @@ var (
 		ConstantinopleBlock:   big.NewInt(7280000),
 		PetersburgBlock:       big.NewInt(7280000),
 		IstanbulBlock:         big.NewInt(9069000),
-		EWASMBlock:            nil,
 		EIP1559Block:          nil,
 		EIP1559FinalizedBlock: nil,
+		EWASMBlock:            nil,
 		Ethash:                new(EthashConfig),
 	}
 
@@ -107,9 +107,9 @@ var (
 		ConstantinopleBlock:   big.NewInt(4230000),
 		PetersburgBlock:       big.NewInt(4939394),
 		IstanbulBlock:         big.NewInt(6485846),
-		EWASMBlock:            nil,
 		EIP1559Block:          nil,
 		EIP1559FinalizedBlock: nil,
+		EWASMBlock:            nil,
 		Ethash:                new(EthashConfig),
 	}
 
@@ -148,9 +148,9 @@ var (
 		ConstantinopleBlock:   big.NewInt(3660663),
 		PetersburgBlock:       big.NewInt(4321234),
 		IstanbulBlock:         big.NewInt(5435345),
-		EWASMBlock:            nil,
 		EIP1559Block:          nil,
 		EIP1559FinalizedBlock: nil,
+		EWASMBlock:            nil,
 		Clique: &CliqueConfig{
 			Period: 15,
 			Epoch:  30000,
@@ -190,9 +190,9 @@ var (
 		ConstantinopleBlock:   big.NewInt(0),
 		PetersburgBlock:       big.NewInt(0),
 		IstanbulBlock:         big.NewInt(1561651),
-		EWASMBlock:            nil,
 		EIP1559Block:          nil,
 		EIP1559FinalizedBlock: nil,
+		EWASMBlock:            nil,
 		Clique: &CliqueConfig{
 			Period: 15,
 			Epoch:  30000,
@@ -239,8 +239,8 @@ var (
 	TestRules = TestChainConfig.Rules(new(big.Int))
 
 	// EIP1559 test configs
-	EIP1559ChainConfig          = &ChainConfig{big.NewInt(1), big.NewInt(0), nil, false, big.NewInt(0), common.Hash{}, big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), nil, big.NewInt(0), nil, new(EthashConfig), nil}
-	EIP1559FinalizedChainConfig = &ChainConfig{big.NewInt(1), big.NewInt(0), nil, false, big.NewInt(0), common.Hash{}, big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), nil, big.NewInt(0), big.NewInt(0), new(EthashConfig), nil}
+	EIP1559ChainConfig          = &ChainConfig{big.NewInt(1), big.NewInt(0), nil, false, big.NewInt(0), common.Hash{}, big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), nil, nil, new(EthashConfig), nil}
+	EIP1559FinalizedChainConfig = &ChainConfig{big.NewInt(1), big.NewInt(0), nil, false, big.NewInt(0), common.Hash{}, big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0), nil, new(EthashConfig), nil}
 
 	EIP1559TestRules          = EIP1559ChainConfig.Rules(new(big.Int))
 	EIP1559FinalizedTestRules = EIP1559FinalizedChainConfig.Rules(new(big.Int))
@@ -312,9 +312,9 @@ type ChainConfig struct {
 	ConstantinopleBlock   *big.Int `json:"constantinopleBlock,omitempty"`   // Constantinople switch block (nil = no fork, 0 = already activated)
 	PetersburgBlock       *big.Int `json:"petersburgBlock,omitempty"`       // Petersburg switch block (nil = same as Constantinople)
 	IstanbulBlock         *big.Int `json:"istanbulBlock,omitempty"`         // Istanbul switch block (nil = no fork, 0 = already on istanbul)
-	EWASMBlock            *big.Int `json:"ewasmBlock,omitempty"`            // EWASM switch block (nil = no fork, 0 = already activated)
 	EIP1559Block          *big.Int `json:"eip1559Block,omitempty"`          // EIP1559 switch block (nil = no fork, 0 = already on eip1559)
 	EIP1559FinalizedBlock *big.Int `json:"eip1559FinalizedBlock,omitempty"` // EIP1559 finalization switch block (nil = no fork, 0 = already on eip1559 finalized)
+	EWASMBlock            *big.Int `json:"ewasmBlock,omitempty"`            // EWASM switch block (nil = no fork, 0 = already activated)
 
 	// Various consensus engines
 	Ethash *EthashConfig `json:"ethash,omitempty"`
@@ -351,7 +351,7 @@ func (c *ChainConfig) String() string {
 	default:
 		engine = "unknown"
 	}
-	return fmt.Sprintf("{ChainID: %v Homestead: %v DAO: %v DAOSupport: %v EIP150: %v EIP155: %v EIP158: %v Byzantium: %v Constantinople: %v Petersburg: %v Istanbul: %v EWASM: %v EIP1559: %v EIP1559Finalized: %v Engine: %v}",
+	return fmt.Sprintf("{ChainID: %v Homestead: %v DAO: %v DAOSupport: %v EIP150: %v EIP155: %v EIP158: %v Byzantium: %v Constantinople: %v Petersburg: %v Istanbul: %v EIP1559: %v EIP1559Finalized: EWASM: %v %v Engine: %v}",
 		c.ChainID,
 		c.HomesteadBlock,
 		c.DAOForkBlock,
@@ -363,9 +363,9 @@ func (c *ChainConfig) String() string {
 		c.ConstantinopleBlock,
 		c.PetersburgBlock,
 		c.IstanbulBlock,
-		c.EWASMBlock,
 		c.EIP1559Block,
 		c.EIP1559FinalizedBlock,
+		c.EWASMBlock,
 		engine,
 	)
 }
@@ -467,9 +467,9 @@ func (c *ChainConfig) CheckConfigForkOrder() error {
 		{"constantinopleBlock", c.ConstantinopleBlock},
 		{"petersburgBlock", c.PetersburgBlock},
 		{"istanbulBlock", c.IstanbulBlock},
-		{"ewasmBlock", c.EWASMBlock},
 		{"eip1559Block", c.EIP1559Block},
 		{"eip1559FinalizedBlock", c.EIP1559FinalizedBlock},
+		{"ewasmBlock", c.EWASMBlock},
 	} {
 		if lastFork.name != "" {
 			// Next one must be higher number
@@ -523,14 +523,14 @@ func (c *ChainConfig) checkCompatible(newcfg *ChainConfig, head *big.Int) *Confi
 	if isForkIncompatible(c.IstanbulBlock, newcfg.IstanbulBlock, head) {
 		return newCompatError("Istanbul fork block", c.IstanbulBlock, newcfg.IstanbulBlock)
 	}
-	if isForkIncompatible(c.EWASMBlock, newcfg.EWASMBlock, head) {
-		return newCompatError("EWASM fork block", c.EWASMBlock, newcfg.EWASMBlock)
-	}
 	if isForkIncompatible(c.EIP1559Block, newcfg.EIP1559Block, head) {
 		return newCompatError("EIP1559 fork block", c.EIP1559Block, newcfg.EIP1559Block)
 	}
 	if isForkIncompatible(c.EIP1559FinalizedBlock, newcfg.EIP1559FinalizedBlock, head) {
 		return newCompatError("EIP1559Finalized fork block", c.EIP1559FinalizedBlock, newcfg.EIP1559FinalizedBlock)
+	}
+	if isForkIncompatible(c.EWASMBlock, newcfg.EWASMBlock, head) {
+		return newCompatError("EWASM fork block", c.EWASMBlock, newcfg.EWASMBlock)
 	}
 	return nil
 }
@@ -599,7 +599,7 @@ type Rules struct {
 	ChainID                                                 *big.Int
 	IsHomestead, IsEIP150, IsEIP155, IsEIP158               bool
 	IsByzantium, IsConstantinople, IsPetersburg, IsIstanbul bool
-	IsEWASM, IsEIP1559, IsEIP1559Finalized                  bool
+	IsEIP1559, IsEIP1559Finalized, IsEWASM                  bool
 }
 
 // Rules ensures c's ChainID is not nil.
@@ -618,8 +618,8 @@ func (c *ChainConfig) Rules(num *big.Int) Rules {
 		IsConstantinople:   c.IsConstantinople(num),
 		IsPetersburg:       c.IsPetersburg(num),
 		IsIstanbul:         c.IsIstanbul(num),
-		IsEWASM:            c.IsEWASM(num),
 		IsEIP1559:          c.IsEIP1559(num),
 		IsEIP1559Finalized: c.IsEIP1559Finalized(num),
+		IsEWASM:            c.IsEWASM(num),
 	}
 }
